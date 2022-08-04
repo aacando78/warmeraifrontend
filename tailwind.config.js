1c8507092b4e22e74e7525ae9e11e490e595e1f6
aacando78 <aacando78@gmail.com>
2022-08-04 17:35:24 +0530

    updated tailwind js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+// To enable dark mode for only single utility class:
+variants: {
+  backgroundColor: ['dark', 'dark-hover', 'dark-group-hover', 'dark-even', 'dark-odd']
+},
+  plugins: [require('tailwindcss-dark-mode')()],
 }
